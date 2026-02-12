c4d512960464846aa04a5888d5a13a175fadd9c3
Griffin Crane <166270386+grc4kd@users.noreply.github.com>
2026-02-12 16:01:55 -0600

    fix: tweak script configuration and added logging function

--- scrape_web_data.sh
+++ scrape_web_data.sh
@@ -6,6 +6,11 @@
 # 3. marshall/format HTML for markdown input file --> used later as a prompt/query to LLM.
 # returns: a string containing formatted markdown text, obtained by downloading and parsing a single web page's text contents
 
+# script functions
+log() {
+  printf "$(date '+%Y-%m-%d %H:%M:%S') - %s\n" "$1"
+}
+
 # Check runtime dependencies
 HAS_CURL=0
 HAS_HTMLQ=0
@@ -13,14 +18,14 @@ CAN_WRITE=0
 
 # requires curl
 if [[ ! $(which curl) ]]; then
-  printf "This application requires curl to run, but it was not found.\n"
+  log "$(printf "This application requires curl to run, but it was not found.\n")"
 else
   HAS_CURL=1
 fi
 
 # requires htmlq
 if [[ ! $(which htmlq) ]]; then
-  printf "This application requires htmlq to run, but it was not found.\n"
+  log "$(printf "This application requires htmlq to run, but it was not found.\n")"
 else
   HAS_HTMLQ=1
 fi
@@ -37,15 +42,16 @@ if [[ $EC1 == 0 ]]; then
   # navigate to next directory, suppressing STDOUT from pushd
   pushd "$SCRAPE_DIR" 1>/dev/null
 else
-  printf "Unable to create temporary directory. Exit code %s from 'mkdir %s'\n" "$EC1" "$SCRAPE_DIR"
+  log "$(printf "Unable to create temporary directory. Exit code %s from 'mkdir %s'\n" "$EC1" "$SCRAPE_DIR")"
   # use home directory instead as a fallback working directory
   pushd "$HOME" 1>/dev/null
 fi
 
 # set other shell variables for this script
-declare DOWNLOAD_URL='https://nextjs.org/docs/app/getting-started/installation'
-declare DOWNLOAD_FILE="$PWD/download_webpage.html"
-declare FORMAT_FILE="$PWD/formatted_webpage.html"
+declare DOWNLOAD_URL='https://owasp.org/Top10/2025/'
+declare HTMLQ_CSS_SELECTOR='span.md-ellipsis'
+declare DOWNLOAD_FILE="$PWD/owasp_Top10_2025_down.html"
+declare FORMAT_FILE="$PWD/owasp-top10-2025.html"
 
 # check that the current working directory can be written to
 touch "$DOWNLOAD_FILE"
@@ -61,35 +67,29 @@ if [[ $CAN_WRITE == 1 && $HAS_CURL == 1 && $HAS_HTMLQ == 1 ]]; then
   # send a GET request with POST data for tool, indicating that
   # this request is from the `curl` program.
   curl --get --data "tool=curl" \
-    --retry-delay 5 --retry 7 "$DOWNLOAD_URL" >"$DOWNLOAD_FILE"
+    --max-time 30 --retry 7 "$DOWNLOAD_URL" \
+    >"$DOWNLOAD_FILE"
 fi
 
 # check that the results are non-empty
 if [[ ! -s $DOWNLOAD_FILE ]]; then
-  printf "%s" "ERROR: The file: $DOWNLOAD_FILE is empty." 1>&2
+  log "$(printf "%s" "ERROR: The file: $DOWNLOAD_FILE is empty.")" 1>&2
 fi
 
 # format the webpage using `htmlq`
 # select only the paragraph elements using a CSS selector
-cat "$DOWNLOAD_FILE" | htmlq -Bpwt -- 'p' >"$FORMAT_FILE"
+cat "$DOWNLOAD_FILE" | htmlq -Bpwt -- "$HTMLQ_CSS_SELECTOR" >"$FORMAT_FILE"
 EC3=$?
 
 # print terminating statements
 if [[ $EC3 == 0 ]]; then
-  printf "%s\n" "SUCCESS: The file $FORMAT_FILE was created."
+  log "$(printf "%s\n" "SUCCESS: The file $FORMAT_FILE was created.")"
 else
-  printf "%s\n" "ERROR: The file '$FORMAT_FILE' was not created / htmlq failed with error. Exit code: $EC3" 1>&2
+  log "$(printf "%s\n" "ERROR: The file '$FORMAT_FILE' was not created / htmlq failed with error. Exit code: $EC3" 1>&2)"
 fi
-
-# print exit codes / all command statuses
-echo "exit codes"
-printf "%s\t" "EC1(mkdir) |" "EC2(touch) |" "EC3(cat|htmlq)"
-echo
-printf "%s\t" " EC1: $EC1    |" "EC2: $EC2     |" "EC3: $EC3"
-echo
 
 # return to original directory, suppressing STDOUT from popd
 popd 1>/dev/null
 
 # cleanup shell variables
-unset DOWNLOAD_URL DOWNLOAD_FILE FORMAT_FILE
+unset DOWNLOAD_URL HTMLQ_CSS_SELECTOR DOWNLOAD_FILE FORMAT_FILE
