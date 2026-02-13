acfaefb21794044d7cbb77accff02d6ca69848c8
Griffin Crane <166270386+grc4kd@users.noreply.github.com>
2026-02-12 20:12:54 -0600

    fix: check all files when running shellharden script

--- shellharden_scrape-web-data.sh
+++ shellharden_scrape-web-data.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/bash
 
-SCRIPT_FILE=./scrape_web_data.sh
+# change to the script directory first
+SCRIPT_PATH=$(readlink -f "/proc/$$/cwd")
+cd "$SCRIPT_PATH"
 
-HARD_SCRIPT=$(shellharden --transform "$SCRIPT_FILE") && echo "$HARD_SCRIPT" | diff -s - "$SCRIPT_FILE"
+declare -A SOURCEFILES
+SOURCE_FILES=("scrape_web_data.sh" "shellharden_scrape-web-data.sh")
+for srcfile in "${SOURCE_FILES[@]}"; do
+  HAS_SUGGESTION=$(shellharden --check "$SCRIPT_PATH/$srcfile")
+  if [[ $HAS_SUGGESTION == 2 ]]; then
+    echo "WARNING: suggested changes waiting for review."
+  elif [[ $HAS_SUGGESTION == 1 ]]; then
+    echo "ERROR: error while running automation script"
+    echo "Script file path is '$SCRIPT_PATH'"
+    break
+  fi
 
-shellharden --check "$SCRIPT_FILE"
-SHEXIT=$?
+  # exits with no output if all files check out
+done
 
-if [[ $SHEXIT == 0 ]]; then
-  echo "OK: shellharden did not suggest any changes."
-fi
-
-if [[ $SHEXIT == 2 ]]; then
-  echo "ERROR: shellharden suggested the changes above."
-fi
+# change back to last directory quietly
+cd - 1>/dev/null
